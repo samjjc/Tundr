5656cb0e22a5ca79ef75a655c85bcc14b2a8edb3
samjjc <sam111jc@gmail.com>
2016-12-03 10:21:10 -0500

    IT FINALLY DOESN'T CRASHgit add .git add .git add .git add .git add .git add .git add .!

--- app/src/main/java/com/example/johnny/tundr/ShowLocation.java
+++ app/src/main/java/com/example/johnny/tundr/ShowLocation.java
@@ -1,20 +1,29 @@
 package com.example.johnny.tundr;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
-public class ShowLocation extends AppCompatActivity {
+public class ShowLocation extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    protected GoogleApiClient mGoogleApiClient;
+    private GoogleApiClient mGoogleApiClient;
     double mLatitudeText;
     double mLongitudeText;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_location);
         if (mGoogleApiClient == null) {
@@ -25,17 +34,35 @@ public class ShowLocation extends AppCompatActivity {
                     .build();
 
         }
+    }
 
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
 
     }
 
-    /*protected void onStart() {
-        mGoogleApiClient.connect();
-        super.onStart();
-    }*/
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 
 
-    /*public void GetLocation(View view){
+    public void GetLocation(View view){
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
